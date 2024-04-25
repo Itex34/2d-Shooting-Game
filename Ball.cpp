88f0f65d7b423c4ea487668ba5f4837cd435ce6a
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-04-25 10:44:49 +0200

    Create Ball.cpp

---
+++ Ball.cpp
@@ -0,0 +1,49 @@
+#include "Ball.hpp"
+#include <cmath>
+
+Ball::Ball(){}
+
+void Ball::update(int windowWidth, int windowHeight, float deltaTime) {
+
+    double deltaX = velocityX * deltaTime * 200;
+    double deltaY = velocityY * deltaTime * 200;
+
+    offsetCorrectionX = width / 2;
+    offsetCorrectionY = height / 2;
+
+    time += deltaTime;
+
+
+
+    ballY += deltaX;
+    std::cout << "Square coordinates : (x = ";
+    std::cout << ballX;
+    std::cout << ", y = ";
+    std::cout << ballY;
+    std::cout << ")" << std::endl;
+}
+
+void Ball::render(SDL_Renderer* renderer, int x, int y) {
+    x += ballX;
+    y += ballY;
+    if (texture == nullptr) {
+        SDL_Surface* imageSurface = IMG_Load("ball.png");
+        if (imageSurface == NULL) {
+            printf("Unable to load image: %s\n", IMG_GetError());
+            return;
+        }
+        texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+        SDL_FreeSurface(imageSurface);
+    }
+
+    // Check if texture is valid
+    if (texture == nullptr) {
+        printf("Failed to create texture: %s\n", SDL_GetError());
+        return;
+    }
+
+    // Render ball
+    SDL_Rect rect{ x, y, width, height };
+    SDL_RenderCopy(renderer, texture, nullptr, &rect);
+}
+
