ed63ec3dcd3422ef278e2e8e430d37b36844c9d0
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-04-27 12:24:17 +0200

    Update Gun.hpp
    Added a few variables

--- Gun.hpp
+++ Gun.hpp
@@ -5,17 +5,16 @@
 #include "SDL_image.h"
 #include <iostream>
 #include <string>
-
+class Game;
 class Gun {
 public:
     ~Gun();
 
     Gun();
-    void update(int windowWidth, int windowHeight,Game* game);
-    void render(SDL_Renderer* renderer);
-    void shoot(Game* game);
-    void cleanup(Game* game);
-
+    void update(int windowWidth, int windowHeight);
+    void render(SDL_Renderer* renderer, int windowWidth, int windowHeight);
+    void shoot(int windowWidth, int windowHeight);
+    void cleanup();
 private:
     int width = 200;
     int height = 200;
@@ -23,10 +22,15 @@ private:
 
     int gunX = 550;
     int gunY = 400;
-
+    
+    int pelletOffsetCorrectionX = 0;
+    int pelletOffsetCorrectionY = 0;
+    int pelletX = gunX + pelletOffsetCorrectionX;
+    int pelletY = gunY + pelletOffsetCorrectionY;
     int rotation = 0;
+
     float deltaTime;
-    float time = 0;      // Current time
+    float time = 0;
 
     int mouseX = 0, mouseY = 0;
     int offsetCorrectionX = 0, offsetCorrectionY = 0;
