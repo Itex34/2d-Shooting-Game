deeabbfeb2e31ac70d4363592086703f326c8137
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-04-27 12:23:15 +0200

    Update Gun.cpp
    Made the code functional

--- Gun.cpp
+++ Gun.cpp
@@ -3,38 +3,47 @@
 #include "Game.hpp"
 #include <cmath>
 Pellet* pellet = nullptr;
-
+extern Game* game;
 Gun::Gun() {}
 
 void Gun::update(int windowWidth, int windowHeight) {
+
     Uint32 prevTicks = SDL_GetTicks();
     Uint32 currentTicks = SDL_GetTicks();
     deltaTime = (currentTicks - prevTicks) / 1000.0f;
     prevTicks = currentTicks;
-    // adjust gun position
+
     gunX = windowWidth / 2;
     gunY = windowHeight / 2 + 230;
 
-
-        // get mouse position
     SDL_GetMouseState(&mouseX, &mouseY);
 
-    // calculate angle between gun and mouse
     double dx = mouseX - gunX;
     double dy = mouseY - gunY;
+
     rotationAngle = atan2(dy, dx) * 180 / M_PI;
-
-    // adjust for offset
     rotationAngle += angleOffsetCorrection;
-    //shoot if clicked
-    if (SDL_GetMouseState(nullptr, nullptr) & SDL_BUTTON(SDL_BUTTON_LEFT)) {
-        shoot();
-        std::cout << "click!" << std::endl;
+
+
+
+} 
+
+void Gun::render(SDL_Renderer* renderer, int windowWidth, int windowHeight) {
+    float pelletSpeed = 3.0f;
+    float pelletVelX = pelletSpeed * cos((rotationAngle - 40.1) * M_PI / 180);
+    float pelletVelY = pelletSpeed * sin((rotationAngle - 40.1) * M_PI / 180);
+    //pelletX = 50;
+    //pelletY = 50;
+    Uint32 mouseState = SDL_GetMouseState(&mouseX, &mouseY);
+    if (mouseState & SDL_BUTTON(SDL_BUTTON_LEFT)){
+        pelletX += pelletVelX;
+        pelletY += pelletVelY;
     }
-
-}
-
-void Gun::render(SDL_Renderer* renderer) {
+    if (game != nullptr)
+    {
+        delete pellet;
+        pellet = new Pellet(pelletX, pelletY, pelletVelX, pelletVelY);
+    }
     if (texture == nullptr) {
         SDL_Surface* imageSurface = IMG_Load("shotgun.png");
         if (imageSurface == nullptr) {
@@ -50,10 +59,13 @@ void Gun::render(SDL_Renderer* renderer) {
         SDL_FreeSurface(imageSurface);
     }
 
-
     // calculate rendering position
     int renderX = gunX - width / 2;
     int renderY = gunY - height / 2;
+
+
+    pellet->render(game->getRenderer(), pelletX, pelletY);//here i access the game that is defined in the main.cpp file 
+    pellet->update(pelletX, pelletY);
 
     SDL_Rect rect{ renderX, renderY, width, height };
 
@@ -61,18 +73,9 @@ void Gun::render(SDL_Renderer* renderer) {
     SDL_RenderCopyEx(renderer, texture, NULL, &rect, rotationAngle, nullptr, SDL_FLIP_NONE);
 
 }
-void Gun::shoot() {
-
-    float pelletSpeed = 10.0f;
-    float pelletVelX = pelletSpeed * cos(rotationAngle * M_PI / 180);
-    float pelletVelY = pelletSpeed * sin(rotationAngle * M_PI / 180);
-
-    delete pellet;
-    pellet = new Pellet(gunX, gunY, pelletVelX, pelletVelY);
-
-    SDL_RenderPresent(game->getRenderer());//here i need to access the game that i defined in the main.cpp file
-    pellet->render(game->getRenderer());// here sa well
-
+void Gun::shoot(int windowWidth, int windowHeight) {
+        pellet->render(game->getRenderer(), pelletX, pelletY);//here i access the game that is defined in the main.cpp file 
+        pellet->update(pelletX, pelletY);
 }
 
 void Gun::cleanup() {
@@ -80,8 +83,8 @@ void Gun::cleanup() {
         delete pellet;
         pellet = nullptr;
     }
-    if (gameTwo != nullptr) {
-        delete gameTwo;
-        gameTwo = nullptr;
+    if (game != nullptr) {
+        delete game;
+        game = nullptr;
     }
 }
