772eded4ab7e1bda9886f82f28e54e505da5fdb2
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-05-09 19:46:20 +0200

    Update Gun.hpp

--- Gun.hpp
+++ Gun.hpp
@@ -5,16 +5,18 @@
 #include "SDL_image.h"
 #include <iostream>
 #include <string>
+#include <cmath>
 class Game;
 class Gun {
 public:
     ~Gun();
-
     Gun();
+
     void update(int windowWidth, int windowHeight);
     void render(SDL_Renderer* renderer, int windowWidth, int windowHeight);
     void shoot(int windowWidth, int windowHeight);
     void cleanup();
+    void createPelletTexture(SDL_Renderer* renderer);
 private:
     int width = 200;
     int height = 200;
@@ -22,11 +24,15 @@ private:
 
     int gunX = 550;
     int gunY = 400;
-    
+
     int pelletOffsetCorrectionX = 0;
     int pelletOffsetCorrectionY = 0;
-    int pelletX = gunX + pelletOffsetCorrectionX;
-    int pelletY = gunY + pelletOffsetCorrectionY;
+    float pelletX = 0;
+    float pelletY = 0;
+    float tempPelletVelX = 0;
+    float tempPelletVelY = 0;
+    int gunTipX = 0;
+    int gunTipY = 0;
     int rotation = 0;
 
     float deltaTime;
@@ -34,14 +40,15 @@ private:
 
     int mouseX = 0, mouseY = 0;
     int offsetCorrectionX = 0, offsetCorrectionY = 0;
-    double rotationAngle;
+    double rotationAngle = 0;
     double angleOffsetCorrection = 51;
 
     SDL_Texture* texture = nullptr;
-
+    SDL_Texture* pelletTexture = nullptr;
 
     bool textureLoaded = false;
-
+    bool clicked = false;
+    bool pelletIsVisible = true;
 
 };
 
