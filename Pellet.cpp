14d6a3fc557ed938af6a265ff9a7a0a9d3834bc2
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-05-09 19:44:59 +0200

    Update Pellet.cpp
    updated boundaries check

--- Pellet.cpp
+++ Pellet.cpp
@@ -2,51 +2,41 @@
 #include "SDL.h"
 #include "SDL_image.h"
 
-Pellet::Pellet(int x, int y, float velX, float velY)
-    : posX(x), posY(y), velocityX(velX), velocityY(velY) {
+Pellet::~Pellet() {
+    if (texture != nullptr) {
+        SDL_DestroyTexture(texture);
+        texture = nullptr;
+    }
+}
+Pellet::Pellet(float x, float y, float velX, float velY)
+    : posX(x), posY(y), velocityX(velX), velocityY(velY){
+
 }
 
-void Pellet::update(int x, int y, int windowWidth, int windowHeight) {
+void Pellet::update(float x, float y, int windowWidth, int windowHeight) {
     posX = x;
     posY = y;
-    if (posX > (windowWidth - 100) || posY > (windowHeight - 100)) {
+    std::cout << posX << "\n";
+    std::cout << posY << "\n";
+    if (posX > (windowWidth) || posY > (windowHeight)) {
         isOutOfBoundaries = true;
-        std::cout << "Out of boundaries!" << std::endl;
     }
-    if (posX < 100 || posY < 100) {
+    if (posX < 0 || posY < 0) {
         isOutOfBoundaries = true;
-        std::cout << "Out of boundaries!" << std::endl;
     }
-    std::cout << posX << std::endl;
-    std::cout << posY << std::endl;
 }
 
-void Pellet::render(SDL_Renderer* renderer,int x, int y) {
-    if (texture == nullptr) {
-        SDL_Surface* imageSurface = IMG_Load("ball.png");
-        if (imageSurface == NULL) {
-            printf("Unable to load image: %s\n", IMG_GetError());
-            return;
-        }
-        texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-        SDL_FreeSurface(imageSurface);
-    }
-
-    if (texture == nullptr) {
-        printf("Failed to create pellet texture: %s\n", SDL_GetError());
-        return;
-    }
+void Pellet::render(SDL_Renderer* renderer, float x, float y, SDL_Texture* texture) {
     SDL_Rect rect{ posX, posY, radius, radius };
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderCopy(renderer, texture, nullptr, &rect);
-    SDL_DestroyTexture(texture);
 }
 
-
-void Pellet::deleteTexure(SDL_Texture* texture) {
+void Pellet::deleteTexture(SDL_Texture* texture) {
     SDL_DestroyTexture(texture);
 }
 SDL_Texture* Pellet::getTexture() const {
     return texture;
 }
+
 
