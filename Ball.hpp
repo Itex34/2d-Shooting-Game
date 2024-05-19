e6a70ada451663a2a1f2d3fc1fbca31b21ab21c7
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-05-19 09:54:50 +0200

    Update Ball.hpp

--- Ball.hpp
+++ Ball.hpp
@@ -1,8 +1,8 @@
 #ifndef Ball_hpp
 #define Ball_hpp
 
-#include "SDL.h"
-#include "SDL_image.h"
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
 #include <iostream>
 #include <string>
 
@@ -13,26 +13,28 @@ public:
 
     void update(int windowWidth, int windowHeight, float deltaTime);
 
-    void render(SDL_Renderer* renderer,int x,int y);
-    
+    void render(SDL_Renderer* renderer, int x, int y);
 
+    SDL_Texture* getTexture() const;
 
+    void deleteTexture(SDL_Texture* texture);
 
+    bool isOutOfBoundaries = false;
 private:
     float deltaTime;
     int width = 100;
     int height = 100;
-    int cnt = 0;
 
     int ballX = 200;
     int ballY = 43;
     int velocityX = 2;
     int velocityY = 2;
 
-    float time = 0; 
+    float time = 0;
 
     int mouseX = 0, mouseY = 0;
     int offsetCorrectionX = 0, offsetCorrectionY = 0;
+
     SDL_Texture* texture = nullptr;
 };
 
