9a0a37c82958048c2a6ee5787d083a2fa93d003a
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-05-19 09:50:37 +0200

    Fixed shooting in Gun.cpp

--- Gun.cpp
+++ Gun.cpp
@@ -29,33 +29,55 @@ void Gun::update(int windowWidth, int windowHeight) {
 
 }
 
+
 void Gun::render(SDL_Renderer* renderer, int windowWidth, int windowHeight) {
-    SDL_Event event;
 
-    float pelletSpeed = 20.0f;
-    float pelletVelX = pelletSpeed * cos((rotationAngle - 47) * M_PI / 180);
-    float pelletVelY = pelletSpeed * sin((rotationAngle - 47) * M_PI / 180);
+    float pelletSpeed = 30.0f;
+    float pelletVelX = 0;
+    float pelletVelY = 0;
 
+    pelletVelX = pelletSpeed * cos((initialRotationAngle - 47) * M_PI / 180);
+    pelletVelY = pelletSpeed * sin((initialRotationAngle - 47) * M_PI / 180);
+    
     Uint32 mouseState = SDL_GetMouseState(&mouseX, &mouseY);
 
     gunTipX = gunX + cos((rotationAngle - 60) * M_PI / 180) * 100;
     gunTipY = gunY + sin((rotationAngle - 60) * M_PI / 180) * 100;
-
+    
 
     if (mouseState & SDL_BUTTON(SDL_BUTTON_LEFT)) {
-        if (pellet == nullptr || pellet->isOutOfBoundaries) {
+        if (pellet == nullptr) {
+            pelletIsVisible = true;
             pelletX = gunTipX;
             pelletY = gunTipY;
+            initialRotationAngle = rotationAngle;
         }
-    } 
-    pelletX += pelletVelX;
-    pelletY += pelletVelY;
- 
+    }
+
+
     if (game != nullptr)
     {
-        delete pellet;
-        pellet = new Pellet(pelletX, pelletY, pelletVelX, pelletVelY);
+        if(pellet == nullptr)
+        {
+            pelletIsVisible = true;
+            delete pellet;
+            pellet = new Pellet(pelletX, pelletY, pelletVelX, pelletVelY);
+        }
+
+        if (pellet->isOutOfBoundaries == false)
+        {            
+            shoot(windowWidth, windowHeight, pelletX, pelletY);
+            createPelletTexture(renderer);
+            pelletX += pelletVelX;
+            pelletY += pelletVelY;
+
+        }
+        else {
+            pelletIsVisible = false;
+        }
+
     }
+    
 
     int renderX = gunX - width / 2;
     int renderY = gunY - height / 2;
@@ -76,11 +98,8 @@ void Gun::render(SDL_Renderer* renderer, int windowWidth, int windowHeight) {
         SDL_FreeSurface(imageSurface);
     }
 
-    if (pellet->isOutOfBoundaries == false) {
-        createPelletTexture(renderer);
-        shoot(windowWidth, windowHeight);
-    }
-    else if (pellet != nullptr && pellet->isOutOfBoundaries) {
+
+    else if (pellet != nullptr && pellet->isOutOfBoundaries == true) {
         pellet->deleteTexture(pellet->getTexture());
         delete pellet;
         pellet = nullptr;
@@ -88,11 +107,13 @@ void Gun::render(SDL_Renderer* renderer, int windowWidth, int windowHeight) {
 
 
     SDL_Rect rect{ renderX, renderY, width, height };
-    SDL_RenderCopyEx(renderer, texture, NULL, &rect, rotationAngle, nullptr, SDL_FLIP_NONE);//render the rotated image
+    SDL_RenderCopyEx(renderer, texture, NULL, &rect, rotationAngle, nullptr, SDL_FLIP_NONE);
 }
-void Gun::shoot(int windowWidth, int windowHeight) {
-    pellet->render(game->getRenderer(), pelletX, pelletY, pelletTexture);
-    pellet->update(pelletX, pelletY, windowWidth, windowHeight);
+void Gun::shoot(int windowWidth, int windowHeight, float xPos, float yPos) {
+    xPos = pelletX;
+    yPos = pelletY;
+    pellet->render(game->getRenderer(), xPos, yPos, pelletTexture);
+    pellet->update(xPos, yPos, windowWidth, windowHeight);
 }
 
 void Gun::cleanup() {
@@ -103,19 +124,18 @@ void Gun::cleanup() {
 }
 
 void Gun::createPelletTexture(SDL_Renderer* renderer) {
-    if(pelletIsVisible == true)
-    if (pelletTexture == nullptr) {
+    if (pelletIsVisible == true && pelletTexture == nullptr){   
         SDL_Surface* imageSurface = IMG_Load("ball.png");
         if (imageSurface == NULL) {
             printf("Unable to load image: %s\n", IMG_GetError());
             return;
         }
         pelletTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-        SDL_FreeSurface(imageSurface);
+        SDL_FreeSurface(imageSurface);      
     }
 
     if (pelletTexture == nullptr) {
         printf("Failed to create pellet texture: %s\n", SDL_GetError());
         return;
     }
-}
+}      
