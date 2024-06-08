709f81e2c53a5278d631b34de3a5d55b26a6d568
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-06-08 09:46:32 +0200

    Update Pellet.hpp

--- Pellet.hpp
+++ Pellet.hpp
@@ -7,20 +7,20 @@ class Gun;
 class Pellet {
 public:
     ~Pellet();
-    Pellet(float x, float y, float velX, float velY);
+    Pellet(float x, float y);
     void update(float x, float y, int windowWidth, int windowHieght);
     void render(SDL_Renderer* renderer, float x, float y, SDL_Texture* texture);
     void deleteTexture(SDL_Texture* texture);
     SDL_Texture* getTexture() const;
     bool isOutOfBoundaries = false;// <- this boolean will be set to true if the pellet is out of the "reload boundaries", this way we can shoot again once the bool is true
-                                   //albeit bad, it's basically a way of having a reload time, see how it works in pellet.cpp at lines 21 to 26
-                                   //i couldn't find a better and more precise way of doing this, if you have any idea, tell me on github
+    //albeit bad, it's basically a way of having a reload time, see how it works in pellet.cpp at lines 21 to 26
+    //i couldn't find a better and more precise way of doing this, if you have any idea, tell me on github
+
+    float radius = 20;
 private:
     float posX;
     float posY;
-    float velocityX;
-    float velocityY;
-    float radius = 20;
+
 
     SDL_Texture* texture = nullptr;
 };
