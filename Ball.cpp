8a9ccf1146046db1f98c4f61024ecbc9ddd612e0
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-06-08 09:43:39 +0200

    Update Ball.cpp

--- Ball.cpp
+++ Ball.cpp
@@ -17,30 +17,33 @@ void Ball::update(int windowWidth, int windowHeight, float deltaTime) {
 }
 
 void Ball::render(SDL_Renderer* renderer, int x, int y) {
-    x += ballX;
-    y += ballY;
-    if (x < 0) {
-        isOutOfBoundaries == true;
-    }
-    if (texture == nullptr) {
-        SDL_Surface* imageSurface = IMG_Load("ball.png");
-        if (imageSurface == NULL) {
-            printf("Unable to load image: %s\n", IMG_GetError());
+    if(isHit == false)
+    {
+        x += ballX;
+        y += ballY;
+        if (x < 0) {
+            isOutOfBoundaries = true;
+        }
+        if (texture == nullptr) {
+            SDL_Surface* imageSurface = IMG_Load("ball.png");
+            if (imageSurface == NULL) {
+                printf("Unable to load image: %s\n", IMG_GetError());
+                return;
+            }
+            texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+            SDL_FreeSurface(imageSurface);
+        }
+
+        // Check if texture is valid
+        if (texture == nullptr) {
+            printf("Failed to create texture: %s\n", SDL_GetError());
             return;
         }
-        texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-        SDL_FreeSurface(imageSurface);
-    }
 
-    // Check if texture is valid
-    if (texture == nullptr) {
-        printf("Failed to create texture: %s\n", SDL_GetError());
-        return;
+        // Render ball
+        SDL_Rect rect{ x, y, width, height };
+        SDL_RenderCopy(renderer, texture, nullptr, &rect);
     }
-
-    // Render ball
-    SDL_Rect rect{ x, y, width, height };
-    SDL_RenderCopy(renderer, texture, nullptr, &rect);
 }
 
 SDL_Texture* Ball::getTexture() const {
