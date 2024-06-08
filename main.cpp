87bd9dadcb7d1f7dc43c02cc31e3a4ad410aa003
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-06-08 09:49:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,68 +5,107 @@
 #include "Pellet.hpp"
 #include "windows.h"
 #include <cstdlib>
+#include <random>
+#include <vector>
 
 Game* game = nullptr;
-Ball* ball = nullptr;
 Gun* gun = nullptr;
-
+std::vector<Ball*> balls;
+std::vector<int> ballPositionX;
+std::vector<int> ballPositionY;
 int main(int argc, char* argv[])
 {
-	const float targetFPS = 60.0f; // Target frame rate (in frames per second)
-	const float maxFrameTime = 1.0f / targetFPS; // Maximum allowed time per frame (in s
-	Uint32 prevTicks = SDL_GetTicks();
-	const float dt = 1000.0f / targetFPS; // Desired time per frame (in milliseconds)
-	int windowWidth = 1300;
-	int windowHeight = 700;
+    const float targetFPS = 60.0f;
+    const float maxFrameTime = 1.0f / targetFPS; 
+    Uint32 prevTicks = SDL_GetTicks();
+    const float dt = 1000.0f / targetFPS;
+    int windowWidth = 1300;
+    int windowHeight = 700;
 
+    game = new Game();
+    gun = new Gun();
 
+    game->init("GameWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, false);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> ballX(100, windowWidth - 100);
+    std::uniform_int_distribution<> ballY(0, 50);
+    std::uniform_int_distribution<> ballCount(2, 10);
 
-	ball = new Ball();
-	game = new Game();
-	gun = new Gun();
+    // create multiple balls and add them to the vector
+    for (int i = 0; i < ballCount(gen); ++i) {
+        Ball* ball = new Ball();
+        balls.push_back(ball);
+        ballPositionX.push_back(ballX(gen));
+        ballPositionY.push_back(ballY(gen));
+    }
 
+    while (game->running() == true)
+    {
+        Uint32 currentTicks = SDL_GetTicks();
+        float deltaTime = (currentTicks - prevTicks) / 1000.0f;
+        prevTicks = currentTicks;
 
-	game->init("GameWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, false);
+        game->handleEvents();
+        game->update();
 
-	while (game->running())
-	{
+        for (auto ball : balls) {
+            ball->update(windowWidth, windowHeight, deltaTime);
+        }
 
-		Uint32 currentTicks = SDL_GetTicks();
-		float deltaTime = (currentTicks - prevTicks) / 1000.0f; // Convert milliseconds to seconds
-		prevTicks = currentTicks;
+        gun->update(windowWidth, windowHeight);
 
-		game->handleEvents();
-		game->update();
+        // check for collisions and render balls
+        for (size_t i = 0; i < balls.size(); ++i) {
+            Ball* ball = balls[i];
+            if (gun->pelletX <= ball->ballX + 100 || gun->pelletY <= ball->ballY + 100) {
+                ball->isHit = true;
+                break;
+            }
+            if (gun->pelletX <= ball->ballX - 100 || gun->pelletY <= ball->ballY - 100) {
+                ball->isHit = true;
+                break;
+            }
+            //render balls if there is no collision
+            if (ball->isHit == false) {
+                ball->render(game->getRenderer(), ballPositionX[i], ballPositionY[i]);
+            }
+        }
 
-		ball->update(windowWidth, windowHeight, deltaTime);
+        gun->render(game->getRenderer(), windowWidth, windowHeight);
+        SDL_RenderPresent(game->getRenderer());
 
-		gun->update(windowWidth, windowHeight);
+        game->render();
 
-		ball->render(game->getRenderer(), -20, 30);
-		ball->render(game->getRenderer(), 100, 30);
-		ball->render(game->getRenderer(), 300, 30);
-		ball->render(game->getRenderer(), 800, 30);
+        Uint32 frameTicks = SDL_GetTicks() - currentTicks;
+        if (frameTicks < maxFrameTime * 1000.0f) {
+            SDL_Delay((Uint32)((maxFrameTime * 1000.0f) - frameTicks));
+        }
 
-		gun->render(game->getRenderer(), windowWidth, windowHeight);
-		SDL_RenderPresent(game->getRenderer());
+        // check for balls out of boundaries
+        for (auto it = balls.begin(); it != balls.end();) {
+            if ((*it)->isOutOfBoundaries == true) {
+                (*it)->deleteTexture((*it)->getTexture());
+                delete* it;
+                it = balls.erase(it);
+            }
+            else {
+                ++it;
+            }
+        }
+    }
 
-		game->render();
+    gun->cleanup();
+    game->clean();
 
+    // clean up all balls
+    for (auto ball : balls) {
+        ball->deleteTexture(ball->getTexture());
+        delete ball;
+    }
 
-		Uint32 frameTicks = SDL_GetTicks() - currentTicks;
-		if (frameTicks < maxFrameTime * 1000.0f) {
-			SDL_Delay((Uint32)((maxFrameTime * 1000.0f) - frameTicks));
-		}
-		if (ball->isOutOfBoundaries == true) {
-			ball->deleteTexture(ball->getTexture());
-		}
-	}
+    delete gun;
+    delete game;
 
-	gun->cleanup();
-	game->clean();
-	ball->deleteTexture(ball->getTexture());
-	delete ball;
-	delete game;
-	return 0;
-
+    return 0;
 }
