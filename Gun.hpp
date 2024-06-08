0dfac04c607764e2ad17d4bb43635ce535acf03c
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-06-08 09:45:47 +0200

    Update Gun.hpp

--- Gun.hpp
+++ Gun.hpp
@@ -14,21 +14,29 @@ public:
 
     void update(int windowWidth, int windowHeight);
     void render(SDL_Renderer* renderer, int windowWidth, int windowHeight);
-    void shoot(int windowWidth, int windowHeight, float xPos, float yPos);
     void cleanup();
     void createPelletTexture(SDL_Renderer* renderer);
+    float pelletVelX(float rotationOffset);
+    float pelletVelY(float rotationOffset);
+
 
     int gunX = 550;
     int gunY = 400;
-private:
+
 
     int width = 200;
     int height = 200;
 
     int pelletOffsetCorrectionX = 0;
     int pelletOffsetCorrectionY = 0;
-    float pelletX = 0;
-    float pelletY = 0;
+
+    float pelletSpeed = 40.0f;
+
+    float pelletX = 200000;
+    float pelletY = 200000;
+    float pelletRadius = 20.0f;
+
+
     float timeLimit = 0;
     int gunTipX = 0;
     int gunTipY = 0;
@@ -43,12 +51,15 @@ private:
     double initialRotationAngle;
     double angleOffsetCorrection = 51;
 
-    SDL_Texture* texture = nullptr;
-    SDL_Texture* pelletTexture = nullptr;
+
 
     bool textureLoaded = false;
     bool clicked = false;
     bool pelletIsVisible = true;
+private:
+    SDL_Texture* texture = nullptr;
+    SDL_Texture* pelletTexture = nullptr;
+
 };
 
 #endif // Gun_hpp
